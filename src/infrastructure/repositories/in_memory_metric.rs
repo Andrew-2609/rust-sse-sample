fbaec9666832fb1cbe0de73f7f0bea9171cbfea8
Andrew Monteiro <andrew.agoravai@gmail.com>
2026-01-03 21:42:43 -0300

    feat: use tokio's RwLock due to async nature of repository store

--- src/infrastructure/repositories/in_memory_metric.rs
+++ src/infrastructure/repositories/in_memory_metric.rs
@@ -1,36 +1,36 @@
-use std::{
-    collections::HashMap,
-    sync::{Arc, RwLock},
-};
-
-use async_trait::async_trait;
-use uuid::Uuid;
-
 use crate::domain::{
     entities::metric::MetricEntity, errors::DomainError, repositories::metric::MetricRepository,
 };
 
+use async_trait::async_trait;
+
+use std::{collections::HashMap, sync::Arc};
+
+use tokio::sync;
+
+use uuid::Uuid;
+
 #[derive(Default)]
 pub struct InMemoryMetricRepository {
-    store: Arc<RwLock<HashMap<String, MetricEntity>>>,
+    store: Arc<sync::RwLock<HashMap<String, MetricEntity>>>,
 }
 
 #[async_trait]
 impl MetricRepository for InMemoryMetricRepository {
     async fn create_metric(&self, metric: MetricEntity) -> Result<MetricEntity, DomainError> {
-        let mut map = self.store.write()?;
+        let mut map = self.store.write().await;
         map.insert(metric.id.to_string(), metric.clone());
         Ok(metric)
     }
 
     async fn get_metric_by_id(&self, id: Uuid) -> Result<Option<MetricEntity>, DomainError> {
-        let map = self.store.read()?;
+        let map = self.store.read().await;
         let result = map.get(&id.to_string()).cloned();
         Ok(result)
     }
 
     async fn get_all_metrics(&self) -> Result<Vec<MetricEntity>, DomainError> {
-        let map = self.store.read()?;
+        let map = self.store.read().await;
         let result: Vec<MetricEntity> = map.values().cloned().collect();
         Ok(result)
     }
